3332e15b9b33976851016f33523ec1279c94af09
electro2678 <chobits2678@gmail.com>
2025-08-19 19:44:39 -0700

    files

---
+++ semester 2/IT 120/scripts/All Windows Software.ps1
@@ -0,0 +1,17 @@
+﻿# Combine both 32-bit and 64-bit uninstall registry paths
+$registryPaths = @(
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKCU:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+)
+
+$apps = foreach ($path in $registryPaths) {
+    Get-ItemProperty $path -ErrorAction SilentlyContinue | Where-Object {
+        $_.DisplayName -and $_.DisplayVersion
+    } | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+}
+
+# Output results
+Write-Output "`n--- Installed Applications ---"
+$apps | Sort-Object DisplayName | Format-Table -AutoSize
